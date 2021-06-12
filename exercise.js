1f438ec0014368d636e57181fc9b4a1d956efa4d
Leo <leo.cho1989@gmail.com>
2021-06-11 20:11:49 -0700

    third commit

--- exercise.js
+++ exercise.js
@@ -27,3 +27,37 @@ function sum(...args){
 Function.prototype.myBind = function (ctx, ...bindArgs) {
   return (...callArgs) => this.apply(ctx, bindArgs.concat(callArgs));
 };
+
+function curriedSum(numArgs) {
+  const numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach((n) => { total += n; });
+
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+
+Function.prototype.curry = function (numArgs) {
+  const args = [];
+  const fn = this;
+  function _curriedFn(arg) {
+    args.push(arg);
+    if (args.length === numArgs) {
+      return fn.apply(null, args);
+    } else {
+      return _curriedFn;
+    }
+  }
+  return _curriedFn;
+};
